f4320aa2cf0c210a78c6bbd3485260d1d07abbbf
serwk <serwk@rambler.ru>
2019-07-18 06:26:21 +0300

    3

--- src/HomeWork_1/FromTheTeacher/MainCircles.java
+++ src/HomeWork_1/FromTheTeacher/MainCircles.java
@@ -54,7 +54,6 @@ public class MainCircles extends JFrame {
         }
 
         backGround.setBackground();
-        backGround.setBackground();
     }
 
     private void render(GameCanvas canvas, Graphics g) {
