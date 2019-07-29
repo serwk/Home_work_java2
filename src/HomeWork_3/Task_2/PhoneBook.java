cf229f3833b3044e350d14d61102fa630b21d2de
serwk <serwk@rambler.ru>
2019-07-29 05:10:58 +0300

    Take home work 4

--- src/HomeWork_3/Task_2/PhoneBook.java
+++ src/HomeWork_3/Task_2/PhoneBook.java
@@ -3,5 +3,5 @@ package HomeWork_3.Task_2;
 import java.util.HashMap;
 
 public class PhoneBook {
-    HashMap<Person,S>
+   // HashMap<Person,S>
 }
---
+++ src/HomeWork_4/ClientGUI.java
@@ -0,0 +1,119 @@
+package HomeWork_4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class ClientGUI extends JFrame implements ActionListener, Thread.UncaughtExceptionHandler {
+
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 300;
+
+    private final JTextArea log = new JTextArea();
+    private final JPanel panelTop = new JPanel(new GridLayout(2, 3));
+    private final JTextField tfIPAddress = new JTextField("127.0.0.1");
+    private final JTextField tfPort = new JTextField("8189");
+    private final JCheckBox cbAlwaysOnTop = new JCheckBox("Always on top");
+    private final JTextField tfLogin = new JTextField("ivan");
+    private final JPasswordField tfPassword = new JPasswordField("123");
+    private final JButton btnLogin = new JButton("Login");
+
+    private final JPanel panelBottom = new JPanel(new BorderLayout());
+    private final JButton btnDisconnect = new JButton("<html><b>Disconnect</b></html>");
+    private final JTextField tfMessage = new JTextField();
+    private final JButton btnSend = new JButton("Send");
+
+    private final JList<String> userList = new JList<>();
+
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientGUI();
+            }
+        });
+    }
+
+    private ClientGUI() {
+        Thread.setDefaultUncaughtExceptionHandler(this);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(WIDTH, HEIGHT);
+        setTitle("Chat Client");
+        log.setEditable(false);
+        JScrollPane scrollLog = new JScrollPane(log);
+        JScrollPane scrollUsers = new JScrollPane(userList);
+        String[] users = {"user1", "user2", "user3", "user4", "user5", "User_with_a_very_long_name"};
+        userList.setListData(users);
+        scrollUsers.setPreferredSize(new Dimension(100, 0));
+
+
+        panelTop.add(tfIPAddress);
+        panelTop.add(tfPort);
+        panelTop.add(cbAlwaysOnTop);
+        panelTop.add(tfLogin);
+        panelTop.add(tfPassword);
+        panelTop.add(btnLogin);
+        panelBottom.add(btnDisconnect, BorderLayout.WEST);
+        panelBottom.add(tfMessage, BorderLayout.CENTER);
+        panelBottom.add(btnSend, BorderLayout.EAST);
+
+        cbAlwaysOnTop.addActionListener(this);
+
+        // Task 1 Set the listener for the button
+        btnSend.addActionListener(this);
+        tfMessage.addKeyListener(new NumberKeyListener()); // добавляем слушателя к TextField
+
+        add(panelTop, BorderLayout.NORTH);
+        add(panelBottom, BorderLayout.SOUTH);
+        add(scrollLog, BorderLayout.CENTER);
+        add(scrollUsers, BorderLayout.EAST);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object src = e.getSource();
+        if (src == cbAlwaysOnTop) {
+            setAlwaysOnTop(cbAlwaysOnTop.isSelected());
+        } else if (src == btnSend) { // Task 1 Send button press handler
+            logAppened();
+        } else {
+            throw new RuntimeException("Unknown source: " + src);
+        }
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        e.printStackTrace();
+        String msg;
+        StackTraceElement[] ste = e.getStackTrace();
+        if (ste.length == 0) {
+            msg = "Empty Stacktrace";
+        } else {
+            msg = e.getClass().getCanonicalName() + ": " +
+                    e.getMessage() + "\n\t at " + ste[0];
+        }
+        JOptionPane.showMessageDialog(this, msg, "Exception", JOptionPane.ERROR_MESSAGE);
+        System.exit(1);
+    }
+
+    public void logAppened(){
+        log.append(tfMessage.getText() + "\n");
+        tfMessage.setText("");
+    }
+    public class NumberKeyListener extends KeyAdapter {
+
+        public void keyReleased(KeyEvent event) {
+
+            if (event.getKeyCode() == KeyEvent.VK_ENTER) {
+                logAppened();
+            }
+        }
+    }
+}
+
